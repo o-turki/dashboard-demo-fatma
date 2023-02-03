93eb37d1f26e9a1a94656bb3518cd18b132dc4b2
Othman Turki <o.turki@advantryx.com>
2023-02-03 22:03:28 +0100

    Fix index.php v2: still more fixing is needed

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require_once 'config.php';
 //     exit();
 // }
 
-$query = "SELECT DISTINCT pack_num, pack_qte FROM `p4_pack_operation` WHERE prod_line LIKE '%ISA201%' AND cur_date = '03/02/2023'";
+$query = "SELECT DISTINCT pack_num, pack_qte FROM `p4_pack_operation` WHERE prod_line LIKE '%ISA201%' AND cur_date = DATE_FORMAT(CURRENT_DATE, '%d/%m/%Y')";
 $rslt = $con->query($query);
 
 $tab = [];
@@ -19,7 +19,7 @@ while ($item = $rslt->fetch_assoc()) {
 }
 
 
-$query2 = "SELECT DISTINCT pack_num, qte_fp, qte FROM `p12_control` WHERE prod_line LIKE '%ISA201%' AND cur_day = '03/02/2023'";
+$query2 = "SELECT DISTINCT pack_num, qte_fp, qte FROM `p12_control` WHERE prod_line LIKE '%ISA201%' AND cur_day = DATE_FORMAT(CURRENT_DATE, '%d/%m/%Y')";
 $rslt2 = $con->query($query2);
 
 $tab2 = [];
@@ -38,44 +38,44 @@ while (count($tab) >= $i) {
 }
 
 
-$qdf = 0;
-$qfab = 0;
-$i1 = 0;
-$F = count($tab2);
-while ($F >= $i1) {
-    $qfab += $tab2[$i1]['qte'];
-    $qdf += $tab2[$i1]['qte_fp'];
-    $i1++;
-}
+// $qdf = 0;
+// $qfab = 0;
+// $i1 = 0;
+// $F = count($tab2);
+// while ($F >= $i1) {
+//     $qfab += $tab2[$i1]['qte'];
+//     $qdf += $tab2[$i1]['qte_fp'];
+//     $i1++;
+// }
 
 // // echo $qengaged . '<br>';
 // // echo $qfab . '<br>';
 // // echo $qdf . '<br>';
 
-$query3 = "SELECT operator_reg_num, prod_line, performance FROM `p8_op_performance_h` WHERE prod_line LIKE '%ISA201%' AND cur_date = '03/02/2023'";
-$rslt3 = $con->query($query3);
+// $query3 = "SELECT operator_reg_num, prod_line, performance FROM `p8_op_performance_h` WHERE prod_line LIKE '%ISA201%' AND cur_date = DATE_FORMAT(CURRENT_DATE, '%d/%m/%Y')";
+// $rslt3 = $con->query($query3);
 
-$tab3 = [];
-while ($item3 = $rslt3->fetch_assoc()) {
-    $tab3[] = $item3;
-}
+// $tab3 = [];
+// while ($item3 = $rslt3->fetch_assoc()) {
+//     $tab3[] = $item3;
+// }
 
 // // echo json_encode($tab3);
 
 
-$query4 = "SELECT DISTINCT operator_reg_num, prod_line FROM `p5_presence` WHERE cur_date = '03/02/2023' AND prod_line LIKE '%ISA201%'";
-$rslt4 = $con->query($query4);
-$tab4 = [];
-while ($item4 = $rslt4->fetch_assoc()) {
-    $tab4[] = $item4;
-}
-$p = count($tab4);
-$i2 = 0;
-$performance = 0;
-while (count($tab3) >= $i2) {
-    $performance += $tab3[$i2]['performance'];
-    $i2++;
-}
+// $query4 = "SELECT DISTINCT operator_reg_num, prod_line FROM `p5_presence` WHERE cur_date = DATE_FORMAT(CURRENT_DATE, '%d/%m/%Y') AND prod_line LIKE '%ISA201%'";
+// $rslt4 = $con->query($query4);
+// $tab4 = [];
+// while ($item4 = $rslt4->fetch_assoc()) {
+//     $tab4[] = $item4;
+// }
+// $p = count($tab4);
+// $i2 = 0;
+// $performance = 0;
+// while (count($tab3) >= $i2) {
+//     $performance += $tab3[$i2]['performance'];
+//     $i2++;
+// }
 
 // // json_encode($tab4);
 
@@ -85,23 +85,22 @@ while (count($tab3) >= $i2) {
 
 // // echo " Nombre des paquets encours = ",json_encode( count($tab) - count($tab2)), "\n"; //nombre des Paquets engagés
 
-$qencours = $qengaged - $qfab;
+// $qencours = $qengaged - $qfab;
 
 // // echo "La Quantité encours = ", json_encode($qencours), "\n";
 
 // // echo " La Quantité Fabriquée = ", json_encode($qfab), "\n";
 
-$cq = ($qdf / $qfab) * 100;
+// $cq = ($qdf / $qfab) * 100;
 
 // // echo " Indice de controle qualité = ",  json_encode(number_format($cq,2)), "%";
 
 // // echo "Performance de la chaine = ", json_encode($performance / $p);
 
-
 echo json_encode([
     "qengaged" => $qengaged,
-    "qencours" => $qencours,
-    "qfab" => $qfab,
-    "cq" => $cq,
-    "performance" => $performance,
+    // "qencours" => $qencours,
+    // "qfab" => $qfab,
+    // "cq" => $cq,
+    // "performance" => $performance,
 ]);
