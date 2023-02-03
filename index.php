6204b79f10696c2715f958ac8e65e6b04d84923a
Othman Turki <o.turki@advantryx.com>
2023-02-03 22:27:03 +0100

    Final fix

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 header("content-Type: application/json");
 require_once 'config.php';
 
@@ -9,6 +10,7 @@ require_once 'config.php';
 //     echo "DB connection is failed";
 //     exit();
 // }
+
 
 $query = "SELECT DISTINCT pack_num, pack_qte FROM `p4_pack_operation` WHERE prod_line LIKE '%ISA201%' AND cur_date = DATE_FORMAT(CURRENT_DATE, '%d/%m/%Y')";
 $rslt = $con->query($query);
@@ -27,55 +29,54 @@ while ($item2 = $rslt2->fetch_assoc()) {
     $tab2[] = $item2;
 }
 
-// echo json_encode($tab);
-// echo json_encode($tab2);
 
 $i = 0;
 $qengaged = 0;
-while (count($tab) >= $i) {
+// while (count($tab) >= $i) {
+//     $qengaged += $tab[$i]['pack_qte'];
+//     $i++;
+// }
+while ($i < count($tab)) {
     $qengaged += $tab[$i]['pack_qte'];
     $i++;
 }
 
 
-// $qdf = 0;
-// $qfab = 0;
-// $i1 = 0;
-// $F = count($tab2);
-// while ($F >= $i1) {
-//     $qfab += $tab2[$i1]['qte'];
-//     $qdf += $tab2[$i1]['qte_fp'];
-//     $i1++;
-// }
+$qdf = 0;
+$qfab = 0;
+$i1 = 0;
+$F = count($tab2);
+while ($i1 < $F) {
+    $qfab += $tab2[$i1]['qte'];
+    $qdf += $tab2[$i1]['qte_fp'];
+    $i1++;
+}
 
-// // echo $qengaged . '<br>';
-// // echo $qfab . '<br>';
-// // echo $qdf . '<br>';
 
-// $query3 = "SELECT operator_reg_num, prod_line, performance FROM `p8_op_performance_h` WHERE prod_line LIKE '%ISA201%' AND cur_date = DATE_FORMAT(CURRENT_DATE, '%d/%m/%Y')";
-// $rslt3 = $con->query($query3);
+$query3 = "SELECT operator_reg_num, prod_line, performance FROM `p8_op_performance_h` WHERE prod_line LIKE '%ISA201%' AND cur_date = DATE_FORMAT(CURRENT_DATE, '%d/%m/%Y')";
+$rslt3 = $con->query($query3);
 
-// $tab3 = [];
-// while ($item3 = $rslt3->fetch_assoc()) {
-//     $tab3[] = $item3;
-// }
+$tab3 = [];
+while ($item3 = $rslt3->fetch_assoc()) {
+    $tab3[] = $item3;
+}
 
 // // echo json_encode($tab3);
 
 
-// $query4 = "SELECT DISTINCT operator_reg_num, prod_line FROM `p5_presence` WHERE cur_date = DATE_FORMAT(CURRENT_DATE, '%d/%m/%Y') AND prod_line LIKE '%ISA201%'";
-// $rslt4 = $con->query($query4);
-// $tab4 = [];
-// while ($item4 = $rslt4->fetch_assoc()) {
-//     $tab4[] = $item4;
-// }
-// $p = count($tab4);
-// $i2 = 0;
-// $performance = 0;
-// while (count($tab3) >= $i2) {
-//     $performance += $tab3[$i2]['performance'];
-//     $i2++;
-// }
+$query4 = "SELECT DISTINCT operator_reg_num, prod_line FROM `p5_presence` WHERE cur_date = DATE_FORMAT(CURRENT_DATE, '%d/%m/%Y') AND prod_line LIKE '%ISA201%'";
+$rslt4 = $con->query($query4);
+$tab4 = [];
+while ($item4 = $rslt4->fetch_assoc()) {
+    $tab4[] = $item4;
+}
+$p = count($tab4);
+$i2 = 0;
+$performance = 0;
+while ($i2 < count($tab3)) {
+    $performance += $tab3[$i2]['performance'];
+    $i2++;
+}
 
 // // json_encode($tab4);
 
@@ -85,13 +86,13 @@ while (count($tab) >= $i) {
 
 // // echo " Nombre des paquets encours = ",json_encode( count($tab) - count($tab2)), "\n"; //nombre des Paquets engagés
 
-// $qencours = $qengaged - $qfab;
+$qencours = $qengaged - $qfab;
 
 // // echo "La Quantité encours = ", json_encode($qencours), "\n";
 
 // // echo " La Quantité Fabriquée = ", json_encode($qfab), "\n";
 
-// $cq = ($qdf / $qfab) * 100;
+$cq = ($qdf / $qfab) * 100;
 
 // // echo " Indice de controle qualité = ",  json_encode(number_format($cq,2)), "%";
 
@@ -99,8 +100,8 @@ while (count($tab) >= $i) {
 
 echo json_encode([
     "qengaged" => $qengaged,
-    // "qencours" => $qencours,
-    // "qfab" => $qfab,
-    // "cq" => $cq,
-    // "performance" => $performance,
+    "qencours" => $qencours,
+    "qfab" => $qfab,
+    "cq" => round($cq),
+    "performance" => round($performance / $p),
 ]);
--- script.js
+++ script.js
@@ -12,44 +12,46 @@ async function getEngage() {
   const response = await fetch("http://localhost/dash/DashbordV5/index.php");
   const data = await response.json();
   //console.log(data)
-  console.log(data.qengaged);
-  console.log(document.querySelector("#Qte"));
+  // console.log(data.qengaged);
+  // console.log(document.querySelector("#Qte"));
 
-  // document.querySelector("#Qte").innerHTML = "<h1>" + data.qengaged + "</h1>";
+  document.querySelector("#Qte").innerHTML = "<h1>" + data.qengaged + "</h1>";
 }
 getEngage();
 
-// async function getEncours() {
-//   const response = await fetch("http://localhost/dash/DashbordV5/index.php");
-//   const data = await response.json();
-//   console.log(data);
+async function getEncours() {
+  const response = await fetch("http://localhost/dash/DashbordV5/index.php");
+  const data = await response.json();
+  // console.log(data);
 
-//   console.log(data.qencours);
-//   console.log(document.querySelector("#Encours"));
-// }
-// getEncours();
+  // console.log(data.qencours);
+  // console.log(document.querySelector("#Encours"));
+  document.querySelector("#Encours").innerHTML =
+    "<h1>" + data.qencours + "</h1>";
+}
+getEncours();
 
-// async function getFab(){
-//     const response = await fetch ("http://localhost/digitex_isatest/_API/php/")
-//     const data = await response.json()
-//     console.log(data[0].qfab)
+async function getFab() {
+  const response = await fetch("http://localhost/dash/DashbordV5/index.php");
+  const data = await response.json();
 
-// document.querySelector("#fab").innerHTML = "<h1>" + data[0].qfab + "</h1>"
-// }
-// getFab()
-// async function getControl(){
-//     const response = await fetch ("http://localhost/digitex_isatest/_API/php/")
-//     const data = await response.json()
-//     console.log(data[0].cq)
+  document.querySelector("#fab").innerHTML = "<h1>" + data.qfab + "</h1>";
+}
+getFab();
 
-//     document.querySelector("#cq").innerHTML = "<h1>" + data[0].cq + "</h1>"
-// }
-// getControl()
-// async function getPerformance(){
-//     const response = await fetch ("http://localhost/digitex_isatest/_API/php/")
-//     const data = await response.json()
-//     console.log(data[0].performance)
+async function getControl() {
+  const response = await fetch("http://localhost/dash/DashbordV5/index.php");
+  const data = await response.json();
 
-//     document.querySelector("#perf").innerHTML = "<h1>" + data[0].performance + "</h1>"
-// }
-// getPerformance()
+  document.querySelector("#cq").innerHTML = "<h1>" + data.cq + "</h1>";
+}
+getControl();
+
+async function getPerformance() {
+  const response = await fetch("http://localhost/dash/DashbordV5/index.php");
+  const data = await response.json();
+
+  document.querySelector("#perf").innerHTML =
+    "<h1>" + data.performance + "</h1>";
+}
+getPerformance();
